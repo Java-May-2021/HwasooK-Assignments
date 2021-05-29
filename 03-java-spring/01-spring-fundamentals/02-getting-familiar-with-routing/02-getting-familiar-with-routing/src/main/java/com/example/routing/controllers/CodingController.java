b70aa0ea228be02aa41f348c520247a39835cecf
hkyung00 <hwasookyung@gmail.com>
2021-05-28 23:56:49 -0500

    Routing

---
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/02-getting-familiar-with-routing/src/main/java/com/example/routing/controllers/CodingController.java
@@ -0,0 +1,21 @@
+package com.example.routing.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CodingController {
+	@RequestMapping(value="/coding", method=RequestMethod.GET)
+	public String index() {
+		return "Hello Coding Dojo";
+	}
+	@RequestMapping(value="/coding/python", method=RequestMethod.GET)
+	public String python() {
+		return "Python/Django was awesome!";
+	}
+	@RequestMapping(value="/coding/java", method=RequestMethod.GET)
+	public String java() {
+		return "Java/Spring is better!";
+	}
+}
